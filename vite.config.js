f14f5e7cdf17c99037e22d450e235155542078b2
Pontus Johansson <johanssonp998@gmail.com>
2026-02-04 12:42:18 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+    build: {
+        rollupOptions:{
+            input: {
+                main: resolve(__dirname, "index.html"),
+                sass: resolve(__dirname, "sass.html")
+            }
+        }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            png: {
+                quality: 75
+            },
+            jpg: {
+                quality: 75
+            },
+            jpeg: {
+                quality: 75
+            },
+            webp: {
+                quality: 80
+            },
+            avif: {
+                quality: 65
+            }
+        })
+    ]
+})
