29a036a0cbd956359777bb86a6fc7c38242e3786
Pontus Johansson <johanssonp998@gmail.com>
2026-02-05 17:47:08 +0100

    lägger till JS för mobilmenyn

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,16 @@ addEventListener("DOMContentLoaded", () => {
     toggleBtn.addEventListener("click", ()=>{
         document.body.classList.toggle("dark");
     })
+
+    const mobileBtn = document.querySelector("#hamburger");
+    const mobileNavEl = document.querySelector(".mobile-nav");
+    const closeBtn = document.querySelector("#close-btn");
+
+    mobileBtn.addEventListener("click", ()=>{
+        mobileNavEl.classList.toggle("open");
+    })
+
+    closeBtn.addEventListener("click", ()=>{
+        mobileNavEl.classList.remove("open");
+    })
 })
