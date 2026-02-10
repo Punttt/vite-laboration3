b02a4b72bfcf4053fcb4e9b9e526b913c611f6c0
Pontus Johansson <johanssonp998@gmail.com>
2026-02-10 11:43:29 +0100

    upd JS funktion för hamburgemenyn

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
 addEventListener("DOMContentLoaded", () => { 
 
-    const toggleBtn = document.querySelector("#theme-switch");
-
-    toggleBtn.addEventListener("click", ()=>{
-        document.body.classList.toggle("dark");
-    })
-
     const mobileBtn = document.querySelector("#hamburger");
     const mobileNavEl = document.querySelector(".mobile-nav");
     const closeBtn = document.querySelector("#close-btn");
