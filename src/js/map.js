0d170675c4b32c652920f2130f0f088f6b158589
Pontus Johansson <johanssonp998@gmail.com>
2026-02-27 14:42:30 +0100

    testar lägga till URL

--- src/js/map.js
+++ src/js/map.js
@@ -17,9 +17,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 // Hämta kordinater från Nominatim
 async function searchLocation(query){
-    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&addressdetails=1&limit=1&polygon_geojson=0`;
+    const url = `https://corsproxy.io/?https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&addressdetails=1&limit=1&polygon_geojson=0`;
     const response = await fetch(url);
     const data = await response.json();
 
-    console.table(data);
+    if(data.length === 0){
+        throw new Error("Ingen plats hittades."); 
+    }
+
+    console.log(data[0].lat);
+    console.log(data[0].lon);
 }
