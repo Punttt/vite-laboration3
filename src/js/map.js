9cc1669be1ad819603247e969dddd73b0f25d8c5
Pontus Johansson <johanssonp998@gmail.com>
2026-02-27 10:52:49 +0100

    börjat men funktioner för map, anropa på position

---
+++ src/js/map.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    const form = document.getElementById("locationForm");
+    const input = document.getElementById("locationInput");
+
+    form.addEventListener("submit", async (e)=>{
+        e.preventDefault();
+
+        const query = input.value;
+        console.log(query);
+
+        searchLocation(query);
+    })
+})
+
+
+// Hämta kordinater från Nominatim
+async function searchLocation(query){
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}&addressdetails=1&limit=1&polygon_geojson=0`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.table(data);
+}
